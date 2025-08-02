c333723673c249b308e9ef2296095e60fe7ac56e
Raffaele <mori.raffaele@gmail.com>
2025-08-02 11:03:27 +0100

    Code optimized

--- src/hooks/useThrottledCallback.ts
+++ src/hooks/useThrottledCallback.ts
@@ -2,7 +2,7 @@ import { useRef, useEffect, useCallback } from 'react';
 
 export function useThrottledCallback<T extends (...args: any[]) => void>(
   callback: T,
-  delay: number
+  delay: number,
 ): T {
   const timeoutRef = useRef<number | null>(null);
   const latestCallback = useRef(callback);
@@ -23,13 +23,15 @@ export function useThrottledCallback<T extends (...args: any[]) => void>(
     this: ThisParameterType<T>,
     ...args: Parameters<T>
   ) {
-    if (timeoutRef.current === null) {
-      latestCallback.current.apply(this, args);
-
-      timeoutRef.current = window.setTimeout(() => {
-        timeoutRef.current = null;
-      }, delay);
+    if (timeoutRef.current) {
+      return;
     }
+    latestCallback.current.apply(this, args);
+
+    timeoutRef.current = window.setTimeout(() => {
+      timeoutRef.current = null;
+      latestCallback.current.apply(this, args);
+    }, delay);
   } as T, [delay]);
 
   return throttledCallback;
