5c2c6e9dc8d80397104c8de96fbd621445f397cc
Raffaele <mori.raffaele@gmail.com>
2025-08-02 11:14:37 +0100

    Typo fixed

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ function App() {
     <div className={appStyles.appContainer}>
       <h1>Main Header</h1>
       <Filler startIndex={0} numberOfElements={10} />
-      <StickyDivOnScroll stickyCheckDelay={500}>
+      <StickyDivOnScroll stickyCheckDelay={100}>
         <div className={appStyles.stickyContentStyle}>
-          <h2>This DIV bcame sticky!</h2>
+          <h2>This DIV will become sticky!</h2>
           <p>Scroll up and down to see the effect.</p>
         </div>
       </StickyDivOnScroll>
